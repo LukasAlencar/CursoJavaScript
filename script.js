7225cb5ce34824a8f1d05b3c181b93e18ec17ad6
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-06-28 18:31:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let idade = prompt('Qual a sua idade?');
+let velocidadeDoCarro = 81;
 
+
+if(velocidadeDoCarro > 80){
+    console.log("Está acima da velocidade permitida");
+}else{
+    console.log("Está dentro da velocidade permitida");
+}
