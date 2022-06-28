e70ecedab300396fa568f31fed9aceeefcc3724d
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-06-28 19:20:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let velocidadeDoCarro = 81;
-
-
-if(velocidadeDoCarro > 80){
-    console.log("Está acima da velocidade permitida");
-}else{
-    console.log("Está dentro da velocidade permitida");
+let numeroAleatorio = function(){
+    console.log(Math.random());
 }
+
+numeroAleatorio();
+numeroAleatorio();
