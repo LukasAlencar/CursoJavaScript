3b552436a077c55991a64ec074f2e13aac78cb0e
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-06-30 23:19:45 -0300

    methods

---
+++ indexOfELastIndexOf/script.js
@@ -0,0 +1,12 @@
+
+let nums = [2, 3, 4, 6, 7, 3, 1, 2];
+
+
+//Achar a posição de algum indice no array(a primeira posição);
+console.log(nums.indexOf(2));
+
+//Achar a posição de algum indice no array (A ultima posição);
+console.log(nums.lastIndexOf(2));
+
+console.log(nums.indexOf(7));
+
---
+++ slice/script.js
@@ -0,0 +1,8 @@
+
+let nums = [2, 3, 4, 6, 7, 3, 1, 2];
+
+let corte = nums.slice(2,3);
+
+
+console.log(nums.slice(2,6));
+console.log(corte);
